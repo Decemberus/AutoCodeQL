fcaec1e281c7158c278509544bc78777496c344e
Decemberus <104720270+Decemberus@users.noreply.github.com>
2024-05-11 14:58:58 +0800

    Added more default properties

--- autocodeql.py
+++ autocodeql.py
@@ -26,10 +26,10 @@ if __name__ == '__main__':
     print(bannerText)
     paser = argparse.ArgumentParser("")
     paser.add_argument('-c','--codeql_path',help = "codeql executable path",required= True)
-    paser.add_argument('-j','--java_project_path',help = "the path of java project to be tested",required= True)
-    paser.add_argument('-d','--database_path',help = "The location where the codeql database is generated",required= True)
-    paser.add_argument('-r','--results_path',help= "The path to store the sarif results", default='../result/codeql.json')
-    paser.add_argument('-q','--query_path',help = "The path of the ql query statement",required=True)
+    paser.add_argument('-j','--java_project_path',help = "the path of java project to be tested",default= "example-project/micro_service_seclab")
+    paser.add_argument('-d','--database_path',help = "The location where the codeql database is generated",default= "example-project/micro_service_seclab/qldb")
+    paser.add_argument('-r','--results_path',help= "The path to store the sarif results", default='result/codeql.json')
+    paser.add_argument('-q','--query_path',help = "The path of the ql query statement",default="ql-query-example/micro-qlpack.ql")
 
     args = paser.parse_args()
     codeql_path = args.codeql_path
@@ -39,8 +39,8 @@ if __name__ == '__main__':
     query_path = args.query_path
 
     #构建codeql数据库
-    #databasecreating(codeql_path,database_path,java_project_path)
+    databasecreating(codeql_path,database_path,java_project_path)
     #执行codeql扫描
-    #codeqlquery(codeql_path,database_path,results_path,query_path)
+    codeqlquery(codeql_path,database_path,results_path,query_path)
     #提取扫描结果
     extract_vulnerabilities(results_path)
